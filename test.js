06173de2482714e25f5b2023d1e6ad16175f8a76
Юля Круглова <juliakruglovka@gmail.com>
2022-01-26 12:10:04 +0300

    add_exchange _rates

--- test.js
+++ test.js
@@ -4,20 +4,26 @@
         let resultCurr = document.getElementById("currRes")
         let valueCurr = document.getElementById("currVal")
 
+        
+         fetch('https://api.exchangerate.host/symbols')
+          .then((response) => {
+            return response.json()
+          })
+          .then((data) => {
+            let response = data.symbols
+            for( key in response){
+              valueCurr.options[valueCurr.options.length] = new Option( key, response[key].code);
+              resultCurr.options[resultCurr.options.length]= new Option( key, response[key].code);
+            }           
+          })
+          fetch('https://api.exchangerate.host/latest?base=RUB')
+          .then((response) => {
+            return response.json()
+          })
+          .then((data) => {
+            let repo = data.rates
+            console.log(repo)
+          })
 
-        var requestURL = 'https://api.exchangerate.host/symbols';
-        var request = new XMLHttpRequest();
-        request.open('GET', requestURL);
-        request.responseType = 'json';
-        request.send();
-
-        request.onload = function() {
-          var response = request.response.symbols;
-          for( key in response){
-            valueCurr.options[valueCurr.options.length] = new Option( key, response[key].code);
-            resultCurr.options[resultCurr.options.length]= new Option( key, response[key].code);
-        } 
-      }
-}
-
+}        
 
