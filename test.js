91c8df9af669f9e4a0026531f097ee4cd3428f78
Юля Круглова <juliakruglovka@gmail.com>
2022-01-26 11:12:09 +0300

    add_options

--- test.js
+++ test.js
@@ -4,18 +4,20 @@
         let resultCurr = document.getElementById("currRes")
         let valueCurr = document.getElementById("currVal")
 
-        val1.value = 1
 
-        function count() {
-            var requestURL = `https://api.exchangerate.host/latest?base=${valueCurr.value}&symbols=USD,EUR,RUB&amount=${val1.value}`;
-            var request = new XMLHttpRequest();
-            request.open('GET', requestURL);
-            request.responseType = 'json';
-            request.send();
-    
-            request.onload = function() {
-                var response = request.response.rates;
-                result.value = response[resultCurr.value]
-            }
-        }
+        var requestURL = 'https://api.exchangerate.host/symbols';
+        var request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+
+        request.onload = function() {
+          var response = request.response.symbols;
+          for( key in response){
+            valueCurr.options[valueCurr.options.length] = new Option( key, response[key].code);
+            resultCurr.options[resultCurr.options.length]= new Option( key, response[key].code);
+        } 
+      }
 }
+
+
