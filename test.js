bdfaf4be07f6aafec00e0deb14a97ffa6a4ce635
Юля Круглова <juliakruglovka@gmail.com>
2022-01-27 11:36:07 +0300

    test

--- test.js
+++ test.js
@@ -3,8 +3,17 @@
         let result = document.getElementById("result")
         let resultCurr = document.getElementById("currRes")
         let valueCurr = document.getElementById("currVal")
+        let exchangeRates
 
-        
+        let exchange = function exchange (amount, valueCur, resultCur){
+          return ( Math.round((amount/valueCur*resultCur)*10000)/10000)
+        };
+        // result.addEventListener('input', res(result.value,exchangeRates[resultCurr.value],exchangeRates[valueCurr.value]))
+        // val.addEventListener('input', res(val.value, exchangeRates[valueCurr.value], exchangeRates[resultCurr.value]))
+        // resultCurr.addEventListener('change', res(val.value, exchangeRates[valueCurr.value], exchangeRates[resultCurr.value]))
+        // valueCurr.addEventListener('change', res(val.value, exchangeRates[valueCurr.value], exchangeRates[resultCurr.value]))
+
+
          fetch('https://api.exchangerate.host/symbols')
           .then((response) => {
             return response.json()
@@ -16,23 +25,23 @@
               resultCurr.options[resultCurr.options.length]= new Option( key, abbreviation[key].code);
             }           
           })
-          fetch('https://api.exchangerate.host/latest?base=RUB&places=4')
+          exchangeRates = fetch('https://api.exchangerate.host/latest?base=RUB&places=4')
           .then((response) => {
             return response.json()
           })
           .then((data) => {
-            let exchangeRates = data.rates
-            function formula() {
-              result.value = Math.round((val.value/exchangeRates[valueCurr.value]*exchangeRates[resultCurr.value])*10000)/10000
-            }
-            function inversionFormula() {
-              val.value = Math.round((result.value/exchangeRates[resultCurr.value]*exchangeRates[valueCurr.value])*10000)/10000
-            }
-
-            result.addEventListener('input', inversionFormula)
-            val.addEventListener('input', formula)
-            resultCurr.addEventListener('change', formula)
-            valueCurr.addEventListener('change', formula)
           })
+          console.log(exchangeRates)
+  
 }        
+
+
+
+            // function formula() {
+            //   result.value = Math.round((val.value/exchangeRates[valueCurr.value]*exchangeRates[resultCurr.value])*10000)/10000
+            // }
+            // function inversionFormula() {
+            //   val.value = Math.round((result.value/exchangeRates[resultCurr.value]*exchangeRates[valueCurr.value])*10000)/10000
+            // }
+
 
