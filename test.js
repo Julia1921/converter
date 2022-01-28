2727645c02eb1e295c6a2bd259ad75806a1679f7
Юля Круглова <juliakruglovka@gmail.com>
2022-01-28 17:47:19 +0300

    test_dropdown_list

--- test.js
+++ test.js
@@ -4,11 +4,17 @@
   let resultCurr = document.getElementById("currRes");
   let valueCurr = document.getElementById("currVal");
   let exchangeRates;
+  let dropdownList = document.querySelector('.dropdown_list')
+  let span = document.querySelector('span')
+
+
 
   function exchange(amount, valueCur, resultCur) {
     return Math.round((amount / valueCur) * resultCur * 10000) / 10000;
   }
-
+  function getId(el) {
+    span.textContent = el.id
+  }
   result.addEventListener("input", () => {
     val.value = exchange(
       result.value,
@@ -19,24 +25,24 @@
   val.addEventListener("input", () => {
     result.value = exchange(
       val.value,
-      exchangeRates[valueCurr.value],
-      exchangeRates[resultCurr.value]
+      exchangeRates[span],
+      exchangeRates[span]
     );
   });
-  resultCurr.addEventListener("change", () => {
-    result.value = exchange(
-      val.value,
-      exchangeRates[valueCurr.value],
-      exchangeRates[resultCurr.value]
-    );
-  });
-  valueCurr.addEventListener("change", () => {
-    result.value = exchange(
-      val.value,
-      exchangeRates[valueCurr.value],
-      exchangeRates[resultCurr.value]
-    );
-  });
+  // resultCurr.addEventListener("change", () => {
+  //   result.value = exchange(
+  //     val.value,
+  //     exchangeRates[valueCurr.value],
+  //     exchangeRates[resultCurr.value]
+  //   );
+  // });
+  // valueCurr.addEventListener("change", () => {
+  //   result.value = exchange(
+  //     val.value,
+  //     exchangeRates[valueCurr.value],
+  //     exchangeRates[resultCurr.value]
+  //   );
+  // });
 
   fetch("https://api.exchangerate.host/symbols")
     .then((response) => {
@@ -44,10 +50,16 @@
     })
     .then((data) => {
       let abbreviation = data.symbols;
-      console.log(abbreviation)
       for (key in abbreviation) {
-        valueCurr.options[valueCurr.options.length] = new Option(key, abbreviation[key].code);
-        resultCurr.options[resultCurr.options.length] = new Option(key, abbreviation[key].code);
+        let li = document.createElement('li')
+        li.textContent = abbreviation[key].description
+        li.id = abbreviation[key].code
+        li.classList.add('dropdown_item')
+        li.setAttribute('onclick', 'getId(this)')
+        dropdownList.append(li)
+        console.log(li)
+        // valueCurr.options[valueCurr.options.length] = new Option(key, abbreviation[key].code);
+        // resultCurr.options[resultCurr.options.length] = new Option(key, abbreviation[key].code);
       }
     });
   fetch("https://api.exchangerate.host/latest?base=RUB&places=4")
@@ -57,4 +69,13 @@
     .then((data) => {
       exchangeRates = data.rates;
     });
-}
+
+    let btnDropdown = document.getElementById('btn_dropdown')
+
+    btnDropdown.addEventListener('click', () => {
+      dropdownList.classList.toggle('active')
+    })
+
+
+}   
+ 
