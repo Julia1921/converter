b0e1420bf4295e22096dc8ecd368774ddb00010e
Юля Круглова <juliakruglovka@gmail.com>
2022-01-31 15:19:47 +0300

    result

--- test.js
+++ test.js
@@ -3,46 +3,33 @@
   let result = document.getElementById("result");
   let resultCurr = document.getElementById("currRes");
   let valueCurr = document.getElementById("currVal");
+  let dropdown_one = document.getElementById('dropdown_one')
+  let dropdown_two = document.getElementById('dropdown_two')
+  let spanOne = document.getElementById('spanOne')
+  let spanTwo = document.getElementById('spanTwo')
   let exchangeRates;
-  let dropdownList = document.querySelector('.dropdown_list')
-  let span = document.querySelector('span')
-
-
 
   function exchange(amount, valueCur, resultCur) {
     return Math.round((amount / valueCur) * resultCur * 10000) / 10000;
   }
-  function getId(el) {
+  function getId(el,span) {
     span.textContent = el.id
   }
+
   result.addEventListener("input", () => {
     val.value = exchange(
       result.value,
-      exchangeRates[resultCurr.value],
-      exchangeRates[valueCurr.value]
+      exchangeRates[spanTwo.textContent],
+      exchangeRates[spanOne.textContent]
     );
   });
   val.addEventListener("input", () => {
     result.value = exchange(
       val.value,
-      exchangeRates[span],
-      exchangeRates[span]
+      exchangeRates[spanOne.textContent],
+      exchangeRates[spanTwo.textContent]
     );
   });
-  // resultCurr.addEventListener("change", () => {
-  //   result.value = exchange(
-  //     val.value,
-  //     exchangeRates[valueCurr.value],
-  //     exchangeRates[resultCurr.value]
-  //   );
-  // });
-  // valueCurr.addEventListener("change", () => {
-  //   result.value = exchange(
-  //     val.value,
-  //     exchangeRates[valueCurr.value],
-  //     exchangeRates[resultCurr.value]
-  //   );
-  // });
 
   fetch("https://api.exchangerate.host/symbols")
     .then((response) => {
@@ -55,12 +42,33 @@
         li.textContent = abbreviation[key].description
         li.id = abbreviation[key].code
         li.classList.add('dropdown_item')
-        li.setAttribute('onclick', 'getId(this)')
-        dropdownList.append(li)
-        console.log(li)
-        // valueCurr.options[valueCurr.options.length] = new Option(key, abbreviation[key].code);
-        // resultCurr.options[resultCurr.options.length] = new Option(key, abbreviation[key].code);
+        li.addEventListener('click', () => {
+          getId(li, spanOne)
+          result.value = exchange(
+            val.value,
+            exchangeRates[spanOne.textContent],
+            exchangeRates[spanTwo.textContent]
+          );
+
+        })
+        dropdown_one.append(li)
       }
+      for (key in abbreviation) {
+        let li = document.createElement('li')
+        li.textContent = abbreviation[key].description
+        li.id = abbreviation[key].code
+        li.classList.add('dropdown_item')
+        li.addEventListener('click', () => {
+          getId(li, spanTwo)
+          result.value = exchange(
+            val.value,
+            exchangeRates[spanTwo.textContent],
+            exchangeRates[spanOne.textContent]
+          );
+        })
+        dropdown_two.append(li)
+      }
+
     });
   fetch("https://api.exchangerate.host/latest?base=RUB&places=4")
     .then((response) => {
@@ -70,10 +78,12 @@
       exchangeRates = data.rates;
     });
 
-    let btnDropdown = document.getElementById('btn_dropdown')
+    resultCurr.addEventListener('click', () => {
+      dropdown_two.classList.toggle('active')
+    })
 
-    btnDropdown.addEventListener('click', () => {
-      dropdownList.classList.toggle('active')
+    valueCurr.addEventListener('click', () => {
+      dropdown_one.classList.toggle('active')
     })
 
 
